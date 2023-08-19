ba243f684c17289a55c7d5d352526a4ac4343a85
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-08-19 16:56:10 +0400

    Modified cacheDuplication.js in Closures folder

--- Lesson5_19.08.23/Closures/cacheDuplication.js
+++ Lesson5_19.08.23/Closures/cacheDuplication.js
@@ -1,5 +1,15 @@
 function createCachingFunction() {
-    return (number) => number * 2;
+    const obj = {};
+    
+    return function(number) {
+        if (obj.hasOwnProperty([number]) && obj[number] === number * 2) {
+            return obj[number] + " (cached)";
+        }
+
+        obj[number] = number * 2;
+
+        return number * 2 + " (calculated)";
+    };
 }
 
 const cachedCalculation = createCachingFunction();
