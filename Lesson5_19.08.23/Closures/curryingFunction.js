4a591e5ac12f6b733abfdcce90aec3a29f4854e7
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-08-20 13:57:01 +0400

    Added curryingFunction.js in Closures folder

---
+++ Lesson5_19.08.23/Closures/curryingFunction.js
@@ -0,0 +1,20 @@
+function add(a, b, c) {
+    return a + b + c;
+}
+
+function curry(func) {
+    return function curried(...args) {
+        if (args.length >= func.length) {
+            return func.apply(this, args);
+        } else {
+            return function(...args2) {
+                return curried.apply(this, args.concat(args2));
+            }
+        }        
+    }
+}
+
+const curriedAdd = curry(add);
+console.log(curriedAdd(1)(2)(3));
+console.log(curriedAdd(1, 2)(3));
+console.log(curriedAdd(1, 2, 3));
