01d5e69664a6ebc18b9d266fdaeb6fd1e2e1525b
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-08-31 23:07:17 +0400

    Modified permutations.js file in Recursion folder

--- Lesson5_19.08.23/Recursion/permutations.js
+++ Lesson5_19.08.23/Recursion/permutations.js
@@ -1,41 +1,59 @@
-// Not finished
+// const resultArr = [];
 
-const resultArr = [];
-
-function factorial(number) {
-    if (number === 0 || number === 1) {
-        return 1;
-    }
+// function factorial(number) {
+//     if (number === 0 || number === 1) {
+//         return 1;
+//     }
     
-    return number * factorial(number - 1);
-}
+//     return number * factorial(number - 1);
+// }
 
-function getCountSameLetters(string) {
-    let countSameLetters = 0;
+// function getCountSameLetters(string) {
+//     let countSameLetters = 0;
+
+//     for (let i = 0; i < string.length; i++) {
+//         for (let j = i + 1; j < string.length; j++) {
+//             if (string[i] === string[j]) {
+//                 countSameLetters++;
+//             }
+//         } 
+//     }
+
+//     return countSameLetters;
+// }
+
+// function permutations(string) {
+//     if (resultArr.length === factorial(string.length) / (getCountSameLetters(string) * 2)) {
+//         return resultArr;
+//     }
+// }
+
+// console.log(permutations("hello"));
+
+function getPermutations(string) {
+    if (string.length === 0) {
+        return "Please enter non empty string!";
+    } else if (string.length === 1) {
+        return string;
+    }
+
+    const permutations = [];
 
     for (let i = 0; i < string.length; i++) {
-        for (let j = i + 1; j < string.length; j++) {
-            if (string[i] === string[j]) {
-                countSameLetters++;
-            }
-        } 
+        const currentChar = string[i];
+
+        if (string.indexOf(currentChar) !== i) {
+            continue;
+        }
+
+        const remeinedString = string.slice(0, i) + string.slice(i + 1);
+
+        for (const permutation of getPermutations(remeinedString)) {
+            permutations.push(currentChar + permutation);
+        }
     }
 
-    return countSameLetters;
+    return permutations;
 }
 
-function permutations(string) {
-    if (resultArr.length === factorial(string.length) / (getCountSameLetters(string) * 2)) {
-        return resultArr;
-    }
-
-    // let currentString = string.slice(0, string.length - 2);
-
-    // for (let i = currentString.length; i < string.length; i++) {
-
-    // }
-
-    // return currentString;
-}
-
-console.log(permutations("hello"));
+console.log(getPermutations("hello"));
