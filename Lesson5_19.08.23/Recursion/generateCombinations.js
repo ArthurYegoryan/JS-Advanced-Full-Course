8254baf7aad138d445295d4ccb5168d85952b8e6
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-09-02 08:56:15 +0400

    modified generateCombinations.js in Recursion folder

--- Lesson5_19.08.23/Recursion/generateCombinations.js
+++ Lesson5_19.08.23/Recursion/generateCombinations.js
@@ -1,17 +1,29 @@
-// Not finished
-
 const inputArray = [1, 2, 3, 4];
 
 function generateCombinations(array, length) {
-    const possibleVariants = factorial(array.length) / (factorial(array.length - length) * factorial(length));
+    if (array.length === 0) {
+        return "Input any array!";
+    } else if (length <= 0 || length > array.length) {
+        return "Invalid length!";
+    } else if (length === array.length) {
+        return [array];
+    } else if (length === 1) {
+        return array.map(elem => [elem]);
+    }
+  
 
-    return possibleVariants;
-}
+    const resultArray = [];
 
-function factorial(number) {
-    if (number === 0 || number === 1) {
-        return 1;
+    for (let i = 0; i <= array.length - length; i++) {
+        const currentDigit = array.slice(i, i + 1);
+        const remainedArray = generateCombinations(array.slice(i + 1), length - 1);
+
+        for (let j = 0; j < remainedArray.length; j++) {
+            resultArray.push(currentDigit.concat(remainedArray[j]));
+        }
     }
 
-    return number * factorial(number - 1);
+    return resultArray;
 }
+
+console.log(JSON.stringify(generateCombinations(inputArray, 2)));
