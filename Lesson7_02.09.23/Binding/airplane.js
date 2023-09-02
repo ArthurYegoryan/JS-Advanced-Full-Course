38372b5fee782bea0d430b20eedf79b8f1aad41f
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-09-02 09:45:48 +0400

    Modified airplane.js in Binding folder

--- Lesson7_02.09.23/Binding/airplane.js
+++ Lesson7_02.09.23/Binding/airplane.js
@@ -1,6 +1,14 @@
 const airplane = {
     name: "Pegas",
 
+    getName: function() {
+        return this.name;
+    },
+
+    setName: function(newName) {
+        this.name = newName;
+    },
+
     isFlying: false,
 
     takeOff: function() {
