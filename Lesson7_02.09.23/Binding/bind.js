22dbe7e359f72d0c7de488dda78be3955e3cc146
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2023-09-07 18:19:43 +0400

    Added bind.js in Binding folder

---
+++ Lesson7_02.09.23/Binding/bind.js
@@ -0,0 +1,20 @@
+const info = {
+    name: "Arthur",
+};
+
+function getInfo(surname, age) {
+    console.log(`Name: ${this.name}, Surname: ${surname}, Age: ${age}`);
+}
+
+function bind(func, context, ...args) {
+    if (context.hasOwnProperty(func)) {
+        return "Invalid binding!";
+    }
+
+    context[func] = func;
+
+    return context[func](...args);
+}
+
+getInfo("Yegoryan", 26);
+bind(getInfo, info, "Yegoryan", 26);
