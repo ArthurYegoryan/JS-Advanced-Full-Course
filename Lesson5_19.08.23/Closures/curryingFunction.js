7a16af3061d051175a7ed6d059429599cd93f9f5
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2023-09-07 09:20:44 +0400

    Modified curryingFunction.js

--- Lesson5_19.08.23/Closures/curryingFunction.js
+++ Lesson5_19.08.23/Closures/curryingFunction.js
@@ -3,15 +3,26 @@ function add(a, b, c) {
 }
 
 function curry(func) {
-    return function curried(...args) {
+    // return function curried(...args) {
+    //     if (args.length >= func.length) {
+    //         return func.apply(this, args);
+    //     } else {
+    //         return function(...args2) {
+    //             return curried.apply(this, args.concat(args2));
+    //         }
+    //     }        
+    // }
+    function curried(...args) {
         if (args.length >= func.length) {
-            return func.apply(this, args);
+            return func(...args);
         } else {
-            return function(...args2) {
-                return curried.apply(this, args.concat(args2));
+            return function (...remainedArgs) {
+                return curried(...args, ...remainedArgs);
             }
-        }        
+        }
     }
+    
+    return curried;
 }
 
 const curriedAdd = curry(add);
