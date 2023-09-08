825d38f902e47c21c807b624f7c9ee0c9630684a
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2023-09-08 16:36:09 +0400

    Modified apply.js in Binding folder

--- Lesson7_02.09.23/Binding/apply.js
+++ Lesson7_02.09.23/Binding/apply.js
@@ -8,16 +8,14 @@ function getInfoApply(argsArray) {
 }
 
 function apply(func, context, args) {
-    return function() {
-        if (context.hasOwnProperty(func)) {
-            return "Invalid binding";
-        }
-
-        context[func] = func;
-
-        return context[func](args);
+    if (context.hasOwnProperty(func)) {
+        return "Invalid binding";
     }
+
+    context[func] = func;
+
+    return context[func](args);
 }
 
 getInfoApply(["Yegoryan", 26]);
-apply(getInfoApply, info, ["Yegoryan", 26])();
+apply(getInfoApply, info, ["Yegoryan", 26]);
