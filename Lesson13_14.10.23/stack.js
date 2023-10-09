024bb43afed65e3bfdb9d0a4c9c6372a18dc3f00
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2023-10-09 20:49:39 +0400

    fix(stack.js): add implementation with Linked List

--- Lesson13_14.10.23/stack.js
+++ Lesson13_14.10.23/stack.js
@@ -1,28 +1,104 @@
 // Implementation with array
 
-class Stack {
-    #stackStorage = [];
+// class Stack {
+//     #stackStorage = [];
 
-    push(elem) {
-        this.#stackStorage.push(elem);
+//     push(elem) {
+//         this.#stackStorage.push(elem);
         
+//         return this;
+//     }
+
+//     pop() {
+//         return this.#stackStorage.pop();
+//     }
+
+//     top() {
+//         if(this.#stackStorage.length) {
+//             return this.#stackStorage[this.#stackStorage.length - 1];
+//         }
+
+//         return "Stack is empty!";
+//     }
+
+//     isEmpty() {
+//         if (this.#stackStorage.length) {
+//             return false;
+//         }
+
+//         return true;
+//     }
+
+//     getSize() {
+//         return this.#stackStorage.length;
+//     }
+// }
+
+// const stack = new Stack();
+// stack.push(1);
+// stack.push(2);
+// console.log(stack.pop());
+// console.log(stack.top());
+// console.log(stack.isEmpty());
+// console.log(stack.getSize());
+
+
+// Implementation with Linked List
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    push(data) {
+        if (!this.tail) {
+            this.head = data;
+            this.tail = data;
+        } else {
+            this.tail.next = data;
+            this.tail = data;
+        }
+
+        this.size += 1;
+
         return this;
     }
 
     pop() {
-        return this.#stackStorage.pop();
+        if (!this.tail) {
+            return "Stack is empty!";
+        }
+
+        let currentNode = this.head;
+
+        while(currentNode.next.next) {
+            currentNode = currentNode.next;
+        }
+
+        const prevTail = currentNode.next;
+        currentNode.next = null;
+        this.tail = currentNode;
+
+        this.size -= 1;
+
+        return prevTail["data"];
     }
 
     top() {
-        if(this.#stackStorage.length) {
-            return this.#stackStorage[this.#stackStorage.length - 1];
-        }
-
-        return "Stack is empty!";
+        return this.tail["data"];
     }
 
     isEmpty() {
-        if (this.#stackStorage.length) {
+        if (this.size) {
             return false;
         }
 
@@ -30,13 +106,20 @@ class Stack {
     }
 
     getSize() {
-        return this.#stackStorage.length;
+        return this.size;
     }
 }
 
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+
 const stack = new Stack();
-stack.push(1);
-stack.push(2);
+
+stack.push(node1);
+stack.push(node2);
+stack.push(node3);
+
 console.log(stack.pop());
 console.log(stack.top());
 console.log(stack.isEmpty());
