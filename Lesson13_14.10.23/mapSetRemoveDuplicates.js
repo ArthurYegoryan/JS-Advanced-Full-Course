9c9b158c8d048699aeec23bd8ca560579b142d8e
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-10-14 13:53:53 +0400

    fix(mapSetRemoveDuplicates.js): add 2-nd version of the task solution

--- Lesson13_14.10.23/mapSetRemoveDuplicates.js
+++ Lesson13_14.10.23/mapSetRemoveDuplicates.js
@@ -1,5 +1,14 @@
+// Version 1
+
+// function removeDyplicates(array) {
+//     return array.filter((item, index) => array.indexOf(item) === index);
+// }
+
+
+// Version 2
+
 function removeDyplicates(array) {
-    return array.filter((item, index) => array.indexOf(item) === index);
+    return Array.from(new Set(array));
 }
 
 console.log(removeDyplicates([6, 4, 8, 6, 8, 5, 4, 0]));
