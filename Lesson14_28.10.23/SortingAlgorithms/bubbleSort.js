16ebad20f04bbe0dba0c6e159c7b13226faaf9ee
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-10-14 17:28:18 +0400

    fix(bubbleSort.js): fix swap moment

--- Lesson14_28.10.23/SortingAlgorithms/bubbleSort.js
+++ Lesson14_28.10.23/SortingAlgorithms/bubbleSort.js
@@ -3,13 +3,11 @@ const array = [15, 6, 8, 3, 9, 0, 7, 2];
 function bubbleSort(array) {
     let noSwap = true;
 
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length - 1; i++) {
         for (let j = 0; j < array.length - 1; j++) {
             if (array[j] > array[j + 1]) {
                 noSwap = false;
-                let temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+                [array[j], array[j + 1]] = [array[j + 1], array[j]];
             }
         }
 
